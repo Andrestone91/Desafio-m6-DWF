9f649c457f587f34a1552e5ba8b85cc98e535a34
Andres <andrestone9191@gmail.com>
2023-01-03 17:17:30 -0300

    game ready

--- server/firebase.ts
+++ server/firebase.ts
@@ -2,17 +2,7 @@ import * as admin from "firebase-admin"
 import * as serviceAccount from "../key.json"
 
 admin.initializeApp({
-    credential: admin.credential.cert({
-        type: "service_account",
-        project_id: "desafio-m6-dwf",
-        private_key_id: process.env.PRIVATE_KEY_ID,
-        private_key: process.env.PRIVATEE_KEY,
-        client_id: "105262542658674613639",
-        auth_uri: "https://accounts.google.com/o/oauth2/auth",
-        token_uri: "https://oauth2.googleapis.com/token",
-        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-oqp3g%40desafio-m6-dwf.iam.gserviceaccount.com"
-    } as any),
+    credential: admin.credential.cert(serviceAccount as any),
     databaseURL: "https://desafio-m6-dwf-default-rtdb.firebaseio.com"
 });
 
